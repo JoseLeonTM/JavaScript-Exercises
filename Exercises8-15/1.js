e353bfcdfb352be9f45f0fa4633d6e3fb50ac2cb
Jose Leon <jleon@tekmexico.com>
2016-04-12 11:37:34 -0700

    First exercise complete

--- Exercises8-15/1.js
+++ Exercises8-15/1.js
@@ -2,48 +2,84 @@
  * Created by Jose Leon on 4/11/2016.
  */
 (function notes(){
-    var id=1;
     var container = document.getElementById('noteHolder');
-    var createbutton = document.getElementById('newNote');
+    var createButton = document.getElementById('newNote');
     if(window.addEventListener) {
-        createbutton.addEventListener('click', createNote, false);
+        createButton.addEventListener('click', createNote, false);
         container.addEventListener('click',noteEvents,false);
     }
     else if(window.attachEvent) {
-        button.attachEvent('click', createNote);
+        createButton.attachEvent('click', createNote);
         container.attachEvent('click',noteEvents);
     }
-    //console.log(window.localStorage.getItem('Note '+3));
-    console.log(window.localStorage);
+    createButton.disabled='';
+    //////////////////////////////////GENERATE NOTES IF THERE ARE IN STORE/////////////////////
     (function readyNotes(){
-        var index=window.localStorage.length;
         if(window.localStorage.length){
-            for(var i in window.localStorage) {
-                if(window.localStorage.hasOwnProperty(i) && i!='length') {
-                    //console.log(typeof window.localStorage.getItem('Note '+index));
-                    //container.appendChild(window.localStorage.getItem('Note '+index));
-                }
-            }
+            createNote(window.localStorage);
         }
     })();
-    function createNote(){
-        var fragment=document.getElementsByTagName('template')[0].content;
-        var newNote=fragment.querySelector('div');
-        newNote.setAttribute('class','note');
-        newNote.dateCreated= new Date();
-        newNote.dateModified=new Date();
-        newNote.id=id.toString();
-        //console.log(newNote);
-        newNote.querySelector('h4').textContent="Note "+parseInt(id++);
-        newNote.querySelector('.editNote').disabled='true';
-        //console.log(newNote.querySelector('.editNote').disabled);
-        newNote.querySelector('.noteContent').contentEditable='true';
-        var stringObject=JSON.stringify(newNote['content']);
-        window.localStorage.setItem(newNote.querySelector('h4').textContent,newNote);
-        container.appendChild(newNote.cloneNode(true));
+    /////////////////////////////////////////NOTE GENERATION/////////////////////////////////
+    function createNote(notes){
+        /////////////////////////////////CHECK WHAT IDS ARE FREE///////////////////////////////
+        var id = 1;
+        function findNewID(){
+            if(window.localStorage.getItem(id.toString())){
+                id++;
+                findNewID();
+            }
+            else return id;
+        }
+        findNewID();
+        ////////////////////////GENERATE A HASH IN CASE NO STORED NOTES ARE SENT///////////////////
+        if(!notes) {
+            notes = {
+                1:""
+            };
+        }
+        for(var i in notes) {
+            if(notes.hasOwnProperty(i)) {
+                var fragment = document.getElementsByTagName('template')[0].content;
+                var newNote = fragment.querySelector('div').cloneNode(true);
+                if(!notes.length) {
+                    console.log("New");
+                    //////////////////////CREATE A NEW NOTE/////////////////////////////
+                    newNote.id = (id++).toString();
+                    createButton.disabled='true';
+                    newNote.querySelector('.editNote').disabled = 'true';
+                    newNote.querySelector('.noteContent').contentEditable = 'true';
+                    //newNote.info = {
+                    //    id: newNote.id,
+                    //    text: newNote.querySelector('.noteContent').textContent,
+                    //    created: new Date(),
+                    //    modified: new Date()
+                    //};
+                    //var stringObject = JSON.stringify(newNote.info);
+                    //window.localStorage.setItem(newNote.info.id, stringObject);
+                }
+                /////////////////////////GENERATE STORED NOTES////////////////////////
+                else{
+                    console.log("Stored");
+                    var storedInfo=JSON.parse(notes[i]);
+                    newNote.id=storedInfo.id;
+                    newNote.querySelector('.noteContent').contentEditable='false';
+                    newNote.querySelector('.noteContent').textContent=storedInfo.text;
+                    newNote.querySelector('.saveNote').style.display='none';
+                    newNote.info = {
+                        id: storedInfo.id,
+                        text: storedInfo.text,
+                        created: storedInfo.created,
+                        modified: storedInfo.modified
+                    };
+                }
+                newNote.querySelector('h4').textContent = "Note " + newNote.id;
+                console.log(window.localStorage);
+                container.appendChild(newNote);
+            }
+        }
     }
+    ////////////////////////////EVENT HANDLER FOR EDITING AND DELETING///////////////////////////
     function noteEvents(note){
-        //console.log(note.target);
         function getNoteBody(target){
             if(target.getAttribute('class')=='note') return target;
             if(target.parentNode.getAttribute('class')=='note') return target.parentNode;
@@ -54,30 +90,41 @@
         var content=noteBody.querySelector('.noteContent');
         var saveButton=noteBody.querySelector('.saveNote');
         var editButton=noteBody.querySelector('.editNote');
-        //console.log(note.target);
         //////////////////////////////////////EDIT NOTES///////////////////////////////////
         if(note.target.getAttribute('class')=="editNote"){
-            content.setAttribute('editable','true');
-            content.setAttribute('autofocus','true');
+            content.setAttribute('contentEditable','true');
             saveButton.style.display="block";
-            //editButton.disabled='false';
+            editButton.disabled='true';
+            createButton.disabled='true';
         }
         //////////////////////////////////////SAVE CHANGES/////////////////////////////////////
         if(note.target.getAttribute('class')=="saveNote"){
             editButton.setAttribute('disabled','false');
-            editButton.disabled='false';
-            //console.log(editButton.disabled);
-            content.setAttribute('contentEditable','false');
+            editButton.disabled='';
+            createButton.disabled='';
             saveButton.style.display="none";
+            content.setAttribute('contentEditable','false');
+            if(!noteBody.info){
+                noteBody.info = {
+                    id: noteBody.id,
+                    text: noteBody.querySelector('.noteContent').textContent,
+                    created: new Date(),
+                    modified: new Date()
+                };
+            }
+            else {
+                noteBody.info.modified = new Date();
+                noteBody.info.text = content.textContent;
+            }
+            window.localStorage.setItem(noteBody.info.id,JSON.stringify(noteBody.info));
+            console.log(window.localStorage);
         }
         //////////////////////////////////DELETE NOTES////////////////////////////////////
         if(note.target.getAttribute('class')=="deleteNote"){
-            window.localStorage.removeItem(noteBody.querySelector('h4').textContent);
+            window.localStorage.removeItem(noteBody.info.id);
+            //window.localStorage.clear();
             console.log(window.localStorage);
-            window.localStorage.clear();
-            //console.log(noteBody.id);
-            //console.log("children: ",container.childNodes);
-            container.removeChild(document.getElementById(noteBody.id));
+            container.removeChild(document.getElementById(noteBody.info.id));
         }
     }
 })();
